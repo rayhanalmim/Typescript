b2f7504f55e37159a4e6dd187b603c096d79f947
rayhanalmim <rayhanalmim1@gmail.com>
2024-05-03 20:09:06 +0600

    learning typescript

--- modele1/src/index.ts
+++ modele1/src/index.ts
@@ -1,14 +1,20 @@
 let course : string = 'next level web development';
 console.log(course);
 
-const obj : {
+type Datatype = {
     name : string;
     age: number;
     id: string;
-} = {
+}
+
+const obj : Datatype = {
     name : "rayhan",
     age: 25,
     id: "222"
+}
+
+const add = (num1: number, num2: number): number =>{
+    return num1+num2;
 }
 
 console.log(obj.name);
