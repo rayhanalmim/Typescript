b0d50f3d7e2586a646836c7e0ab9a127c3be95d4
rayhanalmim <rayhanalmim1@gmail.com>
2024-05-03 20:18:11 +0600

    obj with function in typescript

--- modele1/src/index.ts
+++ modele1/src/index.ts
@@ -23,4 +23,18 @@ const arr1 : string[] = ['rayhan', "al", "mim"];
 
 const studentData : [string, number] = ['rayhan', 25]
 
+//object with functions
+
+const gubAccounts : {
+    accountName: string;
+    blance : number;
+    addBlance: any;
+} = {
+    accountName: 'rayhan',
+    blance: 0,
+    addBlance(amount: number){
+        return this.blance+amount;
+    }
+}
+
 console.log(obj.name);
