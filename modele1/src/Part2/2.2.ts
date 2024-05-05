5737f4ae69914999b9c74e4cff9162cb096c576b
rayhanalmim <rayhanalmim1@gmail.com>
2024-05-05 16:18:22 +0600

    learn genetic with reuseable conditions

--- modele1/src/Part2/2.2.ts
+++ modele1/src/Part2/2.2.ts
@@ -2,11 +2,14 @@
     //Genetic
     type GeneticArr = Array<string>;
 
-    const myData : GeneticArr = ['rayhann', "al", "mim"];
+    const myData: GeneticArr = ['rayhann', "al", "mim"];
 
     console.log(myData);
 
-    
+    //make it reuseable
+    type GeneticArr2<T> = Array<T>;
+    const IdList : GeneticArr2<number> = [3, 4, 6]
+    const IdList2 : GeneticArr2<string> = ["3", "4", "6"]
 
     //
 }
