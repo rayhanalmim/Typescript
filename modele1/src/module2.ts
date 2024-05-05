da50fbc26dcfca4f863d8ecf9de308469743f137
rayhanalmim <rayhanalmim1@gmail.com>
2024-05-05 16:07:10 +0600

    write example of type assertions

--- modele1/src/module2.ts
+++ modele1/src/module2.ts
@@ -2,5 +2,21 @@
     //2nd part for typescript
 
 
+    //Type assertion, type nrrowtion
+
+    const checkType = (params: number | string | undefined): number | string | undefined => {
+        if (typeof params === "number") {
+            return params;
+        }
+        else if (typeof params === 'string') {
+            return params.toString();
+        }
+        else {
+            return params;
+        }
+    }
+    const result = checkType(23) as number;
+    console.log(result);
+
     //
 }
