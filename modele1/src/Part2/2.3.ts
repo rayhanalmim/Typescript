82be164fbe98841fee28f83f4642a50dd8634514
rayhanalmim <rayhanalmim1@gmail.com>
2024-05-05 16:39:30 +0600

    learning functions with genetic

--- modele1/src/Part2/2.3.ts
+++ modele1/src/Part2/2.3.ts
@@ -1,5 +1,10 @@
 {
     // genetic with functions
-
+    const createArrayWithGenetic = (params : string) : String[] =>{
+        return [params];
+    }
+    const result = createArrayWithGenetic('rayhan');
+    // const result2 = createArrayWithGenetic(44);  //give error for defferent type value
+    console.log(result);
     //
 }
