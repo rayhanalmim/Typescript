b7f5313ee94368fa48f90bc540738b5b981a5cd4
rayhanalmim <rayhanalmim1@gmail.com>
2024-05-06 17:20:56 +0600

    creating a class with a method

--- modele1/src/part3(OOP)/3.1.ts
+++ modele1/src/part3(OOP)/3.1.ts
@@ -1,5 +1,18 @@
 {
     // OOP
+    class Person {
+        name : string;
+        id: number;
 
+        constructor(name: string, id: number){
+            this.name = name;
+            this.id = id;
+        }
+
+        public printId(){
+            console.log(`the id of current student is ${this.id}`);
+        }
+    }
+    
     //
 }
